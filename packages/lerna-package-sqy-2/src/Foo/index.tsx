18379ed946205b0c133bfb2aeaed196822f77db5
songqy <songqy3@126.com>
2021-05-19 17:58:08 +0800

    1

--- packages/lerna-package-sqy-2/src/Foo/index.tsx
+++ packages/lerna-package-sqy-2/src/Foo/index.tsx
@@ -1,3 +1,3 @@
 import React from 'react';
 
-export default ({ title }: { title: string }) => <h1>{title + '1'}</h1>;
+export default ({ title }: { title: string }) => <h1>{title + '111111111'}</h1>;
